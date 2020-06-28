2961b58e416ee69d61f13efde05a92b1e6a2e4c1
mk <munekawa0807@outlook.jp>
2020-06-28 18:24:01 +0900

    Initial Commit

---
+++ secondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondKadaiApp
+//
+//  Created by Keita Munekawa on 2020/06/28.
+//  Copyright © 2020 KM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
